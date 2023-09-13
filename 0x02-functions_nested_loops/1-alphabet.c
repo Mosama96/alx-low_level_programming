74f54c3c0c1ff85f6518c602344819a5cebfbbf4
Mosama96 <mohamedosamasust@gmail.com>
2023-09-13 12:10:37 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,12 @@
  * print_alphabet - prints alphabet
  *
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	int i;
 	for (i = 'a'; i <= 'z'; i++)
 	{
 	_putchar(i);
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
