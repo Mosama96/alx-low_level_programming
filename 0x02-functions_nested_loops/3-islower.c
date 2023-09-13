3487f4683b3813a843be35cd43735af99726ea6c
Mosama96 <mohamedosamasust@gmail.com>
2023-09-13 13:36:21 -0700

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
  * _islower - checks if c is capital or small
+ * Return: returns zero or one
+ * @c: input
  */
 
 int _islower(int c)
 {
-	if (c == 'c')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
